8b762f5b3a90b33e0a98003ba38272960883d1c9
irinaspakovskaa <3533868@gmail.com>
2024-03-25 12:32:40 +0300

    flask simple

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import pickle
-# from flask
+from flask import Flask
+
+app = Flask('baby_sleep')
 model_path = 'models/baseline_pipline.pkl'
 
 
+@app.route('/baby_sleep', methods=['GET'])
 def read_model():
     with open(model_path, 'rb') as f_in:
         model = pickle.load(f_in)
-    return model
+    return 'Hi'
 
 
 if __name__ == '__main__':
     model = read_model()
+    app.run(debug=True, host='0.0.0.0', port=9696)
 
 
