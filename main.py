6d94ad261e79e82b91739f24083c7ff56959669c
irinaspakovskaa <3533868@gmail.com>
2024-03-25 12:22:09 +0300

    read model

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+import pickle
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+model_path = 'models/baseline_pipline.pkl'
 
 
-# Press the green button in the gutter to run the script.
+def read_model():
+    with open(model_path, 'rb') as f_in:
+        model = pickle.load(f_in)
+    return model
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    model = read_model()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
